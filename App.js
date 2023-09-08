f0ad15e72d57c7e8be607b4730b430eb31e61279
krishna Namdev <110086276+krishan190@users.noreply.github.com>
2023-09-08 13:29:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+
+// import ClassCmp from "./ClassCmp";
+// import MyCmp from "./MyCmp";
+// import FunCmp from './FunCmp';
+// import CallFun from "./CallFun";
+
+import UseHook from "./UseHook";
+
+// import Hook from "./Hook";
+
+// import Should from "./Should";
+
+// import Mount from "./Mount";
+// import Update from "./Update";
+
+// import Form from "./Form";
+
+// import HideShow from "./HideShow";
+
+// import Class1 from "./Class1";
+// import User from "./User";
+
+// import Fun1 from "./Fun1";
+
+// import ClassState from "./ClassState";
+// import FuncState from "./FunState";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>This is a app Component</h1>
+      <MyCmp></MyCmp> */}
+      {/* <ClassCmp></ClassCmp> */}
+      {/* <FunCmp></FunCmp> */}
+      {/* <CallFun></CallFun> */}
+      {/* <FuncState></FuncState> */}
+      {/* <ClassState></ClassState> */}
+      {/* <Fun1></Fun1> */}
+      {/* <Class1></Class1> */}
+      {/* <User></User> */}
+      {/* <HideShow></HideShow> */}
+      {/* <Form></Form> */}
+      {/* <Mount></Mount> */}
+      {/* <Update></Update> */}
+      {/* <Should></Should> */}
+      {/* <Hook></Hook> */}
+      <UseHook></UseHook>
+    </div>
+  );
+}
+
+export default App;
